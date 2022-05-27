8c9888c792f48e4f08ab7320d912248ed5d68072
Harkály Gergő <git@harkalygergo.hu>
2022-05-27 23:03:16 +0200

    kollekció további darabjai rész

--- woocommerce/single-product/related.php
+++ woocommerce/single-product/related.php
@@ -19,7 +19,69 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+?>
 
+<section class="related products">
+    <?php
+    global $product;
+
+    /** @var WC_Product_Query $loop */
+    $sameNameProducts = new WC_Product_Query([
+        'post_type'      => 'product',
+        'post_status' => 'publish',
+		'post__not_in' => [$product->get_id()],
+        'title'    => $product->get_title(),
+        'posts_per_page' => -1
+    ]);
+    $sameNameProducts = $sameNameProducts->get_products();
+	if (!empty($sameNameProducts)) : ?>
+		<h2><span>A kollekció</span> további darabjai</h2>
+			<ul class="related-product cart-btn-below-img">
+		        <?php foreach ( $sameNameProducts as $related_product ) : ?>
+					<li>
+		                <?php
+		                $product1 = wc_get_product( $related_product->get_id() );
+		                if ( $product1->is_on_sale() ) :
+		                    ?>
+							<span class="onsale"><i class="dt-icon-the7-magn-004-12" aria-hidden="true"></i></span>
+		                <?php
+		                endif;
+		                ?>
+						<a class="product-thumbnail" href="<?php echo esc_url( $product1->get_permalink() ); ?>">
+		                    <?php echo $product1->get_image(); ?>
+						</a>
+						<div class="product-content">
+							<a class="product-title" href="<?php echo esc_url( $product1->get_permalink() ); ?>">
+		                        <?php echo $product1->get_name(); ?>
+							</a>
+
+		                    <?php
+		                    $product_cat_name = get_the_terms( $product1->get_id(), 'product_cat' )['0']->name;
+		                    echo '<div class="prod-cat-box"><span class="prod-category">'.$product1->get_categories().'</span>';
+		                    echo '<span class="prod-attribute">'.$product1->get_attribute( 'stilus' ).'</span></div>';
+		                    ?>
+
+							<span class="price"><?php //echo $product->get_price_html(); ?></span>
+
+		                    <?php
+		                    if ( wc_review_ratings_enabled() ) {
+		                        echo wc_get_rating_html( $product1->get_average_rating() );
+		                    }
+		                    //if ( presscore_config()->get( 'product.related.show_cart_btn' ) ) {
+		                    //echo '<div class="woo-buttons">' . dt_woocommerce_get_product_add_to_cart_icon() . '</div>';
+		                    //}
+		                    ?>
+						</div>
+					</li>
+		        <?php endforeach; ?>
+			</ul>
+		<?php endif;
+    wp_reset_postdata();
+	?>
+</section>
+
+
+<?php
 if ( $related_products ) : ?>
 
     <section class="related products">
@@ -29,6 +91,10 @@ if ( $related_products ) : ?>
 		// TODO lábléc menükből
 		// TODO Minta termék kosárba
 		// TODO gyik oldalsáv
+		// TODO https://www.paperstories.hu/termek/em01-0039-c/
+		// TODO function.php function woocommerce_quantity_input
+		// TODO számlázz.hu összekötés
+		// TODO GLS összekötés
 		$heading = apply_filters( 'woocommerce_product_related_products_heading', __( 'Related products', 'the7mk2' ) );
 
 		if ( $heading ) :
